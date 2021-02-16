04cc7dc1c71c1ac4152f14fe2847f2fb4dff9493
Yang Hanlin <mattoncis@hotmail.com>
2021-02-16 23:42:05 +0800

    feat: refine environment information

--- information/core/environment.py
+++ information/core/environment.py
@@ -1,6 +1,11 @@
 import os
 from . import utils, entity
 from typing import Union
+import platform
+import django
+import rest_framework
+import socket
+import ifaddr
 
 
 class Environment(entity.Entity):
@@ -15,6 +20,44 @@ class Environment(entity.Entity):
     @classmethod
     def current(cls) -> 'Environment':
         return cls({
-            'environment_variables': os.environ,
-            'current_instance': utils.current_instance()
+            'instance': utils.current_instance(),
+            'host': {
+                'platform': _current_platform(),
+                'runtime': _current_runtime(),
+                'network': _current_network(),
+                'environment_variables': os.environ,
+            },
         })
+
+
+def _current_platform() -> dict:
+    return {
+        'architecture': platform.machine(),
+        'os': platform.platform(),
+        'processor': platform.processor(),
+    }
+
+
+def _current_runtime() -> dict:
+    return {
+        'python': {
+            'implementation': platform.python_implementation(),
+            'version': platform.python_version(),
+        },
+        'django': django.__version__,
+        'drf': rest_framework.__version__,
+    }
+
+
+def _current_network() -> dict:
+    interfaces = {}
+    for adapter in ifaddr.get_adapters():
+        entry = {}
+        if adapter.name != adapter.nice_name:
+            entry['name'] = adapter.nice_name
+        entry['ips'] = [str(ip.ip) for ip in adapter.ips]
+        interfaces[adapter.name.decode()] = entry
+    return {
+        'hostname': socket.gethostname(),
+        'interfaces': interfaces,
+    }
